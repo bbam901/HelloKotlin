82db60f40da2da8d4704b503413f21800b01db84
B-G Seo <bumku1234@gmail.com>
2021-04-27 11:44:30 +0900

    BMI calculation by Java & Kotlin

---
+++ app/src/main/java/com/example/hellokotlin/BmiKotlinActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hellokotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class BmiKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_view_binding);
+
+        val heightField = findViewById<EditText>(R.id.heightField)
+        val weightField = findViewById<EditText>(R.id.weightField)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        val btnBmi = findViewById<Button>(R.id.btnBmi)
+
+        btnBmi.setOnClickListener {
+            val Height = heightField.getText().toString().toDouble()
+            val Weight = weightField.getText().toString().toDouble()
+
+            val bmi = Weight / Math.pow(Height / 100.0, 2.0)
+
+            txtResult.setText("Your BMI = " + bmi)
+        }
+
+    }
+}
